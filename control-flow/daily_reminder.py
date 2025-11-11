71077d0d1a3a0330250fb3549cc11202451be10a
Kalungi-ops <hannykalungi@gmail.com>
2025-11-11 15:24:29 +0300

    corrected prompts

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,5 +1,5 @@
-Task = input("Enter task description: ")
-Priority = input("(high, medium, low): ").lower()
+Task = input("Enter your task : ")
+Priority = input("Priority (high, medium, low): ").lower()
 Time_Bound = input("Is it time-bound? (yes/no): ").lower()
 reminder = ""
 match Priority:
