b8f88ae5a8dbf7800f20930a93b3cf2b6304a159
Kalungi-ops <hannykalungi@gmail.com>
2025-11-11 15:27:55 +0300

    final

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,4 +1,4 @@
-Task = input("Enter your task : ")
+Task = input("Enter your task: ")
 Priority = input("Priority (high/medium/low): ").lower()
 Time_Bound = input("Is it time-bound? (yes/no): ").lower()
 reminder = ""
