e244423f92dc9e9d551d304ed1ca4b30df88ffd2
Kalungi-ops <hannykalungi@gmail.com>
2025-11-20 10:47:55 +0300

    add arithmetic_operations.py file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1, num2, operation):
+    op = operation.lower()
+
+    if op == 'add':
+        return num1 + num2
+    elif op == 'subtract':
+        return num1 - num2
+    elif op == 'multiply':
+        return num1 * num2
+    elif op == 'divide':
+        if num2 == 0:
+            return "Error: cannot divide by zero"
+        return num1 / num2
+    else:
+        return "Error: Invalid operation"
