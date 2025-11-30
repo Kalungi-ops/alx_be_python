1f7a3bf8d5e5a95454cc383bd72af614928909d3
Kalungi-ops <hannykalungi@gmail.com>
2025-11-30 10:28:25 +0300

    add test file

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,29 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+
+    def test_substract(self):
+        self.assertEqual(self.calc.subtract(5, 2), 3)
+        self.assertEqual(self.calc.subtract(-7, 1), -8)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(5, 2), 10)
+        self.assertEqual(self.calc.multiply(-10, 2), -20)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-10, 2), -5)
+        self.assertEqual(self.calc.divide(10, 0), "Cannot divide number by zero")
+
+if __name__ == '__main__':
+    unittest.main()
