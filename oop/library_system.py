d819a357e0223c46fb32eb072fb9d65c95464e2a
Kalungi-ops <hannykalungi@gmail.com>
2025-12-04 17:14:35 +0300

    add library sytem file

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,16 +2,25 @@ class Book:
     def __init__(self, title:str, author: str):
         self.title = title
         self.author = author
-        
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        return f"{self.title} by {self.author} — {self.file_size}MB (EBook)"
+
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        return f"{self.title} by {self.author} — {self.page_count} pages (Print Book)"
 
 class Library:
     def __init__(self):
@@ -22,5 +31,6 @@ class Library:
         print(f"Added {book}")
     
     def list_books(self):
+        print("\nLibrary Collection:")
         for book in self.books:
-            print(book)
+            print(f"- {book}")
